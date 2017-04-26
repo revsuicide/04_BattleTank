d61c124f72a78c36ba7d40325af90b0bd7de963b
revsuicide <revsuicide@gmail.com>
2017-04-26 21:51:39 +0100

    LE122 - Added functionality for the AIControlledTank to find the PlayerTank

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -6,15 +6,23 @@
 
 void ATankAIController::BeginPlay() {
 	Super::BeginPlay();
-	auto ControlledTank = GetControlledTank();
-	if (ControlledTank) {
-		UE_LOG(LogTemp, Warning, TEXT("The AI is in possesion of: %s"), *ControlledTank->GetName());
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("The AI cannot find Player Tank"));
 	}
 	else {
-		UE_LOG(LogTemp, Warning, TEXT("There is no AI possessed tank"));
+		UE_LOG(LogTemp, Warning, TEXT("The Player Tank is %s"), *(PlayerTank->GetName()));
 	}
 }
 
 ATank* ATankAIController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const {
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) {
+		return nullptr;
+	}
+	return Cast<ATank>(PlayerPawn);
 }
